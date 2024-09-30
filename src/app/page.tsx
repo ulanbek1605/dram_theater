8857f4dad00a12e2bf19a87651b9c17f7e09bbea
Majid <130305364+majidkamilov1@users.noreply.github.com>
2024-09-30 17:12:39 +0600

    commit reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
 import Repertoires from "@/components/repertuaresblock/repertoires/Repertoires";
 import Premieres from "@/components/upcomingPremieres/premieres/Premieres";
 import Gallery from "@/components/posterGallery/gallery/Gallery";
-<<<<<<< HEAD
-// import Newscard from "@/components/posterNews/newsCard/NewsCard";
-export default function Home() {
-  return (
-   <div>
-   <Repertoires />
-   <Premieres />
-   <Gallery />
-   {/* <Newscard /> */}
-   </div>
-=======
+
 export default function Home() {
   return (
     <div>
@@ -19,6 +9,5 @@ export default function Home() {
       <Premieres />
       <Gallery />
     </div>
->>>>>>> 9fa6a394c893445a8ebe3823cf72e248b82a1fa2
   );
 }
